4f8c66d699f57fc4b4730786d04272e2c08e95f7
tony <ajm043@bucknell.edu>
2025-08-20 19:47:06 -0400

    find rotated index

--- src/algorithms/divideAndConquer/findRotatedIndex.ts
+++ src/algorithms/divideAndConquer/findRotatedIndex.ts
@@ -1,20 +1,36 @@
-export function findRotatedIndex(arr: number[], num: number, low = 0, high = arr.length - 1) {
-    let mid = low + Math.floor((high - low) / 2)
-    // if (arr[mid + 1]! < arr[mid]! && num < )
+export function findRotatedIndex(arr: number[], num: number) {
+  const pivot = findPivot(arr);
+
+  if (pivot! > 0 && num >= arr[0]! && num <= arr[pivot! - 1]!) {
+    return binarySearch(arr, num, 0, pivot! - 1);
+  }
+  return binarySearch(arr, num, pivot!, arr.length - 1);
 }
 
-export function findFirstRotate(arr: number[], num: number, low = 0, high = arr.length - 1) {
-    let mid = low + Math.floor((high - low) / 2)
+function binarySearch(arr: number[], num: number, start: number, end: number) {
+  if (arr.length === 0) {
+    return -1;
+  }
+  if (num < arr[start]! || num > arr[end]!) {
+    return -1;
+  }
+}
 
-    if ((mid === 0 || arr[mid - 1]! < arr[mid]!) && (mid === arr.length - 1 || arr[mid]! > arr[mid + 1]!)) {
-        return mid;
-    }
-    if (mid === 0 || arr[mid - 1]! < arr[mid]!) {
-        if (mid === arr.length - 1 || arr[mid]! > arr[mid + 1]!) {
-            return mid;
-        }
-        return findFirstRotate(arr, num, mid + 1, high);
+function findPivot(arr: number[]) {
+  if (arr.length === 1 || arr[0]! < arr[arr.length - 1]!) {
+    return 0;
+  }
+  let start = 0;
+  let end = arr.length - 1;
+
+  while (start <= end) {
+    const mid = Math.floor((start + end) / 2);
+    if (arr[mid]! > arr[mid + 1]!) {
+      return mid + 1;
+    } else if (arr[start]! <= arr[mid]!) {
+      start = mid + 1;
     } else {
-        return mid;
+      end = mid - 1;
     }
+  }
 }
